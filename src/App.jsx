f9e3281b1c67820c7480984e83e3983a4067db0f
pvtrex <zhenrime@gmail.com>
2026-01-06 17:38:37 +0530

    Cocktails page animated parallax

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
+import Coctails from './components/Cocktails.jsx';
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -12,6 +13,7 @@ const App = () => {
 	<main>
 	 <Navbar />
 	 <Hero />
+	 <Coctails/>
 	
 	</main>
  )
