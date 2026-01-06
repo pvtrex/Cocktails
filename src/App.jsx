32642432faf133937f45b89ffc4ab9fdee47b238
pvtrex <zhenrime@gmail.com>
2026-01-06 17:11:48 +0530

    Hero gsap animation

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+ return (
+	<main>
+	 <Navbar />
+	 <Hero />
+	
+	</main>
+ )
+}
+
+export default App
